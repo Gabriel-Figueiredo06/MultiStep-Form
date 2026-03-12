9ac15fe86ee892d85185c79afda37a96a7744dce
Gabriel de Melo Figueiredo <gabrielmf2006@gmail.com>
2026-03-12 12:11:43 -0300

    Formulário MultiStep

---
+++ multiStep_form/src/components/Thansk.jsx
@@ -0,0 +1,40 @@
+import "./Thanks.css";
+
+import {
+  BsFillEmojiHeartEyesFill,
+  BsFillEmojiSmileFill,
+  BsFillEmojiNeutralFill,
+  BsFillEmojiFrownFill,
+} from "react-icons/bs";
+
+const emojiData = {
+  unsatisfied: <BsFillEmojiFrownFill/>,
+  neutral: <BsFillEmojiNeutralFill/>,
+  satisfied: <BsFillEmojiSmileFill/>,
+  very_satisfied: <BsFillEmojiHeartEyesFill/>
+}
+
+const Thansk = ({ data }) => {
+  return (
+    <div className="thanks-container">
+      <h2>Falta pouco</h2>
+      <p>
+        A sua opinião é importante, em breve você terá um cupom de 10% para sua
+        próxima compra.
+      </p>
+
+      <p>Para concluir sua avaliação clique no botão de "enviar" abaixo</p>
+      <h3>Aqui está o resumo da sua avaliação: {data.name}</h3>
+      <p className="review-data">
+        <span>Satisfação do produto:</span>
+        {emojiData[data.review]}
+      </p>
+      <p className="review-data">
+        <span>Comentário:</span>
+        {data.comment}
+      </p>
+    </div>
+  );
+};
+
+export default Thansk;
